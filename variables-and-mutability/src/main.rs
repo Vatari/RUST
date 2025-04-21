d3153dd474e027d3c8403d3f187584b77e92a094
vatari <majestic13@gmail.com>
2025-04-21 09:41:19 +0300

    exercise

---
+++ variables-and-mutability/src/main.rs
@@ -0,0 +1,51 @@
+// fn main() {
+//     let apples = 50;
+//     let oranges = 14 + 6;
+//     let _fruits = apples + oranges;
+//     println!("This year, my garden has {0} apples and {1} oranges. I can't believe i have {0} apples", apples, oranges);
+// }
+
+// fn main() {
+//     let mut gym_reps = 10;
+//     println!("I plan to do {gym_reps} reps");
+    
+//     gym_reps = 15;
+//     println!("I plan to do {gym_reps} reps");    
+// }
+
+// fn main () {
+//     let grams_of_protein = "100.345";
+//     let grams_of_protein = 100.345;
+//     let mut grams_of_protein = 100;
+//     grams_of_protein = 105;
+
+//     println!("{grams_of_protein}");
+// }
+
+// const TAX_RATE: f64 = 7.25;
+
+// fn main() {    
+//     let income: i32 = 10000;
+//     println!("My income is {income} and my tax rate is {TAX_RATE}");
+// }
+// #![allow(unused_variables)]
+// type Meters = i32;
+// fn main() {
+//     let mile_race_length: Meters = 1600;
+//     let two_mile_race_length: Meters = 3200;
+//     //println!("A one mile race is {mile_race_length} meters long and a two mile race is {two_mile_race_length} meters long");
+// }
+const TOUCHDOWN_POINTS: i32 = 6;
+#[allow(unused_variables)]
+fn main() {
+    let season: &str = "Fall";
+    let mut points_scored: i32 = 28;
+    points_scored = 35;
+    
+    let event_time = "06:00";
+    let event_time = 6;
+
+    println!("My fafourite season is {season}. The team scored {points_scored}. The event started at {event_time}. A touchdown is worth {TOUCHDOWN_POINTS} points");
+    
+    let favorite_beverage = "Snapple Apple";
+}
